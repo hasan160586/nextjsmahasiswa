10d3ade3b46f06471a88697cdbc6dd410abb2e59
hasan160586 <22777002@stmik.jayakarta.ac.id>
2023-03-29 22:54:56 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -125,59 +125,59 @@ import type { AppProps } from 'next/app'
 // }
 
 // 6. Conditional rendering
-function Item({ name, isPacked }) {
-  return (
-    <li className="item">
-      {name} {isPacked && '✔'}
-    </li>
-  );
-}
-
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item
-          isPacked={true}
-          name="Space suit"
-        />
-        <Item
-          isPacked={true}
-          name="Helmet with a golden leaf"
-        />
-        <Item
-          isPacked={false}
-          name="Photo of Tam"
-        />
-      </ul>
-    </section>
-  );
-}
-
-// 7. Rendering lists 
-// import { people } from './data.js';
-// import { getImageUrl } from './utils.js';
-
-// export default function List() {
-//   const listItems = people.map(person =>
-//     <li key={person.id}>
-//       <img
-//         src={getImageUrl(person)}
-//         alt={person.name}
-//       />
-//       <p>
-//         <b>{person.name}:</b>
-//         {' ' + person.profession + ' '}
-//         known for {person.accomplishment}
-//       </p>
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked && '✔'}
 //     </li>
 //   );
+// }
+
+// export default function PackingList() {
 //   return (
-//     <article>
-//       <h1>Scientists</h1>
-//       <ul>{listItems}</ul>
-//     </article>
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item
+//           isPacked={true}
+//           name="Space suit"
+//         />
+//         <Item
+//           isPacked={true}
+//           name="Helmet with a golden leaf"
+//         />
+//         <Item
+//           isPacked={false}
+//           name="Photo of Tam"
+//         />
+//       </ul>
+//     </section>
 //   );
 // }
+
+// // 7. Rendering lists 
+// // import { people } from './data.js';
+// // import { getImageUrl } from './utils.js';
+
+// // export default function List() {
+// //   const listItems = people.map(person =>
+// //     <li key={person.id}>
+// //       <img
+// //         src={getImageUrl(person)}
+// //         alt={person.name}
+// //       />
+// //       <p>
+// //         <b>{person.name}:</b>
+// //         {' ' + person.profession + ' '}
+// //         known for {person.accomplishment}
+// //       </p>
+// //     </li>
+// //   );
+// //   return (
+// //     <article>
+// //       <h1>Scientists</h1>
+// //       <ul>{listItems}</ul>
+// //     </article>
+// //   );
+// // }
 
